f0dfef482ff592fdf08a7a0069b31133dc1a5618
JimLau49 <jimmy71471@gmail.com>
2019-02-11 11:00:45 -0500

    made static page  for posting

--- postings/views.py
+++ postings/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import NameForm
 # Create your views here.
 
 def home(request):
     return render(request, 'postings/login.html')
+
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'login.html', {'form': form})
