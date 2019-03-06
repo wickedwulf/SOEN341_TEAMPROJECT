41e2749d9c930c45653afaf62312dfc468e69788
wickedwulf <32551006+wickedwulf@users.noreply.github.com>
2019-03-06 18:47:20 -0500

    --added: user posting history under profile --added: liking and un liking posts and like count --added: user profile picture can be upload and changed --added: most user profile info boxes (adjust under admin atm) --changed: changed code for handling tweet posting --added: put a default picture for user profile --added: default like icon (can be changed later) --changed: changed media folder so it's now site_media/
    Login: admin
    Pass: test1234

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from . import models
+from accounts.models import user_profile
 
 
-
+class EditUserProfile(forms.ModelForm):
+    class Meta:
+        model = models.user_profile
+        fields = ['user_first_name', 'user_last_name', 'user_email', 'user_phone', 'user_country', 'user_city',
+                  'user_website', 'about_user', 'user_profile_picture']
