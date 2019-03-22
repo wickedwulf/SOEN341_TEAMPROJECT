b5a78f56d00385fa4dddbe49051850939a013ad6
wickedwulf <32551006+wickedwulf@users.noreply.github.com>
2019-03-21 21:19:06 -0400

    --added: editing of a tweet --added: replying to a post --added: a mouse over way to view posts --changes: changed the models for replies a bit
    Note: so make sure to migrate.
    
    from pycharm terminal
    1) python manage.py makemigrations
    2) python manage.py migrate
    
    Login: admin
    Pass: test1234

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from . import models
 from emoji import Emoji
+from accounts.models import user_profile
 
 
 class NewTweetForm(forms.ModelForm):
@@ -10,10 +11,12 @@ class NewTweetForm(forms.ModelForm):
         widgets = {'author_id': forms.HiddenInput(), 'tweet_id': forms.HiddenInput(), 'encrypted_content': forms.HiddenInput(),
                    'replies': forms.HiddenInput(), 'favourites': forms.HiddenInput(),
                    'content': forms.Textarea(attrs={'onkeypress': Emoji.names()}), 'show_encrypted': forms.HiddenInput()}
+
     def __init__(self, *args, **kwargs):
         super(NewTweetForm, self).__init__(*args, **kwargs)
         self.fields['content'].label = "Message:"
         self.fields['content_key'].label = "Encryption Key:"
+
 
 class Private_Message_Form(forms.ModelForm):
     class Meta:
@@ -24,3 +27,29 @@ class Private_Message_Form(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(Private_Message_Form, self).__init__(*args, **kwargs)
         self.fields['target_user_id'].label = "To:"
+
+
+class Reply_To_Tweet_Form(forms.ModelForm):
+    class Meta:
+        model = models.Replies_To_Tweet
+        fields = ['tweet_id', 'author_id', 'reply_id', 'content', 'media_attachment']
+        widgets = {'tweet_id': forms.HiddenInput(), 'author_id': forms.HiddenInput(), 'reply_id': forms.HiddenInput()}
+
+    def __init__(self, *args, **kwargs):
+        super(Reply_To_Tweet_Form, self).__init__(*args, **kwargs)
+        self.fields['content'].label = "Message:"
+
+
+class Edit_Post_Form(forms.ModelForm):
+    class Meta:
+        model = models.Twitter_Tweet
+        fields = ['content', 'author_id', 'media_attachment', 'tweet_id', 'encrypted_content', 'content_key', 'replies', 'favourites', 'encrypt_content', 'show_encrypted']
+        widgets = {'author_id': forms.HiddenInput(), 'tweet_id': forms.HiddenInput(), 'encrypted_content': forms.HiddenInput(),
+                   'replies': forms.HiddenInput(), 'favourites': forms.HiddenInput(),
+                   'content': forms.Textarea(attrs={'onkeypress': Emoji.names()}), 'show_encrypted': forms.HiddenInput()}
+
+    def __init__(self, *args, **kwargs):
+        super(Edit_Post_Form, self).__init__(*args, **kwargs)
+        self.fields['content'].label = "Message:"
+        self.fields['content_key'].label = "Encryption Key:"
+    # self.initial['content'] = models.Twitter_Tweet.objects.get(tweet_id=)    user_profile.objects.get(user_name=self.user).user_last_name
