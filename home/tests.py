e7174cc9d1da87a3288cee2469432e704fb1eb26
JimLau49 <jimmy71471@gmail.com>
2019-04-08 10:05:12 -0400

    home url reverse resolve test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from home.views import homeactions
 
-#Create tests here
+
+class TestUrls(TestCase):
+
+    def test_homeactions_is_resolved(self):
+        url = reverse('home:home_actions')
+        self.assertEquals(resolve(url).func, homeactions)
